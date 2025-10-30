1007a2a5661a064135515cf545a0f678b6222f2f
lfreeman5 <122315086+lfreeman5@users.noreply.github.com>
2025-10-30 14:28:12 -0600

    Update verify_2d.py

--- verify_2d.py
+++ verify_2d.py
@@ -7,4 +7,7 @@ if __name__ == "__main__":
     x,y,t = sp.Symbol('x'), sp.Symbol('y'), sp.Symbol('t')
     u_exact = sp.sin(sp.pi*t)*sp.sin(sp.pi*x)*sp.sin(sp.pi*y)
     alpha = 1
-    c = []   
+    c = [-y,x]
+
+    f = sp.diff(u_exact, 't') + c[0]*sp.diff(u_exact,'x') + c[1]*sp.diff(u_exact, 'y') - alpha * (sp.diff(u_exact, 'x', 2) + sp.diff(u_exact, 'y', 2))
+    print(sp.simplify(f))
