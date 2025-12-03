216cf0a7b3de4ed71814c198f230a05fb7b697e0
nhoch11 <nathan.hoch@usu.edu>
2025-12-03 16:37:47 -0700

    fixed indexing on the vel data in ns_main

--- tensor_edition/ns_main.py
+++ tensor_edition/ns_main.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     # Create mass, stiffness matrices, which will be N2 x N2
     # Create 2D u0 
     alpha = .001
-    N = 10
+    N = 5
     pts, wts = gll_pts_wts(N)
     M,A = create_mass_stiffness_2d(N)
     
     k = 3 # time stepping order for BDFk/ABk 
     
     Nt = 100
-    dt = 0.01
+    dt = 0.001
 
     lid_velocity = 0.0
 
@@ -58,11 +58,13 @@ if __name__ == "__main__":
 
         print(f'Calculating iter {n+1}, previous max u: {np.max(vel[n,0,:])}, previous max v: {np.max(vel[n,1,:])}')
         # update v_hat using saved u and v data,   if k=3, (n-k+1):(n+1) should give n-2, n-1, n
-        v_hat = calc_v_hat(k, dt, vel[(n-k+1):(n+1),:, ::-1], M, J_hat, B_M, D_tilde)
+        v_hat = calc_v_hat(k, dt, vel[(n-k+1):(n+1)][::-1], M, J_hat, B_M, D_tilde)
+        print("n, n-1, n-2", vel[(n-k+1):(n+1)][::-1])
+        print("n-2, n-1, n", vel[(n-k+1):(n+1),:,:])
 
         # do pressure solve
         print("Pressure solve")
-        p = pressure_solve(N, k, dt, vel[(n-k+1):(n+1),:, ::-1], v_hat, A, M, Dx, Dy, vel_boundary)
+        p = pressure_solve(N, k, dt, vel[(n-k+1):(n+1)][::-1], v_hat, A, M, Dx, Dy, vel_boundary)
         v_hathat = correct_vhat_with_pressure(N,dt,v_hat, p, Dx, Dy)
 
         # do helmholtz solves
@@ -71,11 +73,11 @@ if __name__ == "__main__":
 
         cfl = evaluate_cfl(N, dt, vel[n+1,:,:])
         print("cfl = ", cfl )
-        
+
         if(n%1==0):
             # Plot the current state every 10 iterations
             u2d = map_1d_to_2d(vel[n+1,0,:], N)
             v2d = map_1d_to_2d(vel[n+1,1,:], N)
-            # plot_ns_solution_2d_vector(u2d, v2d, np.arange(Nt)*dt, pts, (n+1)*dt)
+            plot_ns_solution_2d_vector(u2d, v2d, np.arange(Nt)*dt, pts, (n+1)*dt)
 
-            plot_pressure_2d(map_1d_to_2d(p, N), np.arange(Nt)*dt, pts, (n+1)*dt)
+            # plot_pressure_2d(map_1d_to_2d(p, N), np.arange(Nt)*dt, pts, (n+1)*dt)
